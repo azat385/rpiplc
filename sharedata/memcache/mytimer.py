5be8bccf8fa4df7763ac8336e5e914b636e412bb
azat385 <azat385@gmail.com>
2014-09-01 08:58:00 +0100

    issues with TONOFF

--- sharedata/memcache/mytimer.py
+++ sharedata/memcache/mytimer.py
@@ -16,28 +16,16 @@ FINISH = 1
 DELAY_VALUE_DEFAULT = 5
 # exception classes
 # interface functions
+get_edge = lambda x,y: 1 if x and (not y) else 0 
+get_on = lambda x,y: get_edge(x,y)
+get_off = lambda x,y: get_edge(y,x)
 # classes
-"""
-is not compiled
-class TON(Timer):
-	def __init__(self, *args):
-		args[0]=self.time_delay
-		#self.start(time_delay,act)
-		#self.cancel()
-		return 0
-	def check():
-		return 0 
-	def act(self):
-		
-		return 0
-	pass
-"""
 class TON(object):
 	#name_str = "TON"
 	def __init__(self,input, delay_value = DELAY_VALUE_DEFAULT):
 		self.status = NOT_RUNNING
 		self.input = input
-                self.input_prev = input
+                self.input_prev = 0 #input #!!!important comment
 		self.delay_value = delay_value
 		self.count = 0
 		self.do = 0
@@ -63,7 +51,7 @@ class TON(object):
 	def out(self,input,delay_value = DELAY_VALUE_DEFAULT):
 		self.delay_value = delay_value
 		self.input=input
-		get_edge = lambda x,y: 1 if x and (not y) else 0
+		#get_edge = lambda x,y: 1 if x and (not y) else 0
 		on = get_edge(self.input, self.input_prev)
 		off = get_edge(self.input_prev, self.input)
 		if on:
@@ -91,6 +79,28 @@ class TOFF(TON):
 		self.do = 1 if self.status==RUNNING else 0
 		self.do = self.do or self.di
 		return self.di, self.do, self.count, self.name_str
+	pass
+class TONOFF(TON):
+	def __init__(self,input,delay_value_on = DELAY_VALUE_DEFAULT,delay_value_off = DELAY_VALUE_DEFAULT):
+		self._t1 = TON(input,delay_value_on)
+		self._t1.out(input,delay_value_on)
+		self._t2 = TOFF(self._t1.do,delay_value_off)
+		TON.__init__(self,input)	#just for init
+		return None
+	def out(self,input,delay_value_on = DELAY_VALUE_DEFAULT, delay_value_off = DELAY_VALUE_DEFAULT):
+		self._t1.out(input,delay_value_on)
+		self._t2.out(self._t1.do,delay_value_off)
+		#self.do = self._t2.do
+		if self._t1.status == RUNNING and self._t2.status != RUNNING:
+			self.do = self._t1.do
+		else:
+			self.do = self._t2.do
+		self.di = input
+		return self.di, self.do, self._t1.count, self._t2.count, self.name_str
+	pass
+class TONF(object):
+        def __init__(self,input,delay_value_on = DELAY_VALUE_DEFAULT,delay_value_off = DELAY_VALUE_DEFAULT):
+		return None
 	pass
 class bcolors:
     HEADER = '\033[95m'
@@ -121,7 +131,7 @@ def main():
 	import pylibmc
 	shared = pylibmc.Client(["127.0.0.1"], binary=True)
     	shared.behaviors = {"tcp_nodelay": True, "ketama": True}
-	shared["input_bool"] = 0
+	shared["input_bool"] = 1
 	shared["delay_var"] = 5
 	#-----------
 	print bcolors.WARNING + "Warning: No active frommets remain. Continue?"+ bcolors.ENDC
@@ -132,6 +142,7 @@ def main():
 	t1 = TON(l1)
 	t2 = TOFF(l1)
 	t3 = TOFF(t1.do)
+	t4 = TONOFF(l1)
 	#t3 = childTOFF(l1)
 	while True:
 	    #try:
@@ -140,11 +151,12 @@ def main():
 		#print "t1: in",int(t1.input),"out",t1.out(l1,v1),"t2: in",int(t2.input),"out",t2.out(t1.out(l1,v1)[0],v1),\
 		#	"the sum:", t1.out(l1,v1)[0] or t2.out(t1.out(l1,v1)[0],v1)[0]
 		#print "t3:in", int(t3.input), "out",t3.out(l1,v1),"DI",t3.di, "DO",t3.do
-		for var,i in zip(("t1","t2","t3"),(t1, t2, t3)):
+		for var,i in zip(("t1","t2","t3","t4"),(t1, t2, t3, t4)):
 			#i.out(l1,v1)
 			t1.out(l1,v1)
 			t2.out(l1,v1)
 			t3.out(t1.do,v1)
+			t4.out(l1,4,8)
 			print var,i.out_str(),
 		print
 		time.sleep(1)
